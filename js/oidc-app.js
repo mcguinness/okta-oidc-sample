e7a55a92bc1031b1d9ce60d06ccfbd3a879a8096
Karl McGuinness <dev@karlmcguinness.com>
2016-04-06 10:35:42 -0700

    updated with 1.0 SDK and changed theme

--- js/oidc-app.js
+++ js/oidc-app.js
@@ -2,7 +2,7 @@ requirejs.config({
     "baseUrl": "js",
     "paths": {
       "jquery": "jquery-2.1.4.min",
-      "okta-auth-sdk": "OktaAuthRequireJquery"
+      "okta-auth-sdk": "okta-auth-sdk-1.0.0.min"
     }
 });
 
@@ -10,7 +10,7 @@ define(["jquery", "okta-auth-sdk"], function($, OktaAuth) {
 
   var idp = '0oa5kecjfwuF4HQ4w0h7';
   var client = new OktaAuth({
-    uri: "https://example.oktapreview.com",
+    url: "https://example.oktapreview.com",
     clientId: 'ANRZhyDh8HBFN5abN6Rg',
     redirectUri: window.location.href
   });
@@ -28,63 +28,79 @@ define(["jquery", "okta-auth-sdk"], function($, OktaAuth) {
     });
   };
 
+  var displayError = function(msg) {
+    $('div.error').remove();
+    $('div.login-box').append('<div class="error"><p>'+ msg + '</p></div>');
+  }
+
   $(document).ready(function() {
     $('#btn-sign-in').click(function() {
       resetDisplay();
-      client.current.primaryAuth({
+      client.signIn({
         username: $('#username').val(),
         password: $('#password').val()
-      }).then(function(res) {
-        client.getIdToken({
-          scopes: ['openid', 'email', 'profile', 'phone'],
-          sessionToken: res.sessionToken
-        })
-          .then(function(res) {
-            console.log('id_token: %s', res.id_token);
-            displayClaims(res.claims);
-          })
-          .fail(function(err) {
-            console.log(err);
-            $('div.login-box').append('<div class="error"><p>'+ err.message + '</p></div>');
-          })
+      }).then(function(tx) {
+        switch(tx.status) {
+          case 'SUCCESS':
+            client.idToken.authorize({
+              scopes: ['openid', 'email', 'profile', 'phone'],
+              sessionToken: tx.sessionToken
+            })
+              .then(function(res) {
+                console.log('id_token: %s', res.idToken);
+                displayClaims(res.claims);
+                localStorage.setItem('id_token', res.idToken);
+              })
+              .fail(function(err) {
+                console.log(err);
+                displayError(err.message);
+              })
+            break;
+          default:
+            throw 'We cannot handle the ' + tx.status + ' status';
+        }
+
       }).fail(function(err) {
         console.log(err);
         var message = err.errorCauses.length > 0 ? err.errorCauses[0].errorSummary : err.message;
-        $('div.login-box').append('<div class="error"><p>'+ message + '</p></div>');
+        displayError(message);
       });
     });
 
     $('#btn-idp').click(function() {
       resetDisplay();
-      client.getIdToken({
+      client.idToken.authorize({
         scopes: ['openid', 'email', 'profile', 'phone'],
         prompt: false,
         idp: idp
       })
         .then(function(res) {
-          console.log('id_token: %s', res.id_token);
+          console.log('id_token: %s', res.idToken);
           displayClaims(res.claims);
+          localStorage.setItem('id_token', res.idToken);
         })
         .fail(function(err) {
           console.log(err);
-          $('div.login-box').append('<div class="error"><p>'+ err.message + '</p></div>');
+          displayError(err.message);
         })
     });
 
 
     $('#btn-refresh').click(function() {
       resetDisplay();
-      client.getIdToken({
-        scopes: ['openid', 'email', 'profile', 'phone'],
-        prompt: false
-      })
+      var idToken = localStorage.getItem('id_token');
+      if (!idToken) {
+        return displayError('You must first sign-in before you can refresh a token!');
+      }
+      client.idToken.refresh(idToken)
         .then(function(res) {
-          console.log('id_token: %s', res.id_token);
+          console.log('id_token: %s', idToken);
           displayClaims(res.claims);
         })
         .fail(function(err) {
           console.log(err);
-          $('div.login-box').append('<div class="error"><p>'+ err.message + '</p></div>');
+          displayError(err.message);
+          localStorage.setItem('id_token', null);
         })
     });
   });
